09e7eb46dc0aca7159b5e84aacba975aadd4d212
Yohann Zaoui <yohannzaoui@gmail.com>
2026-01-21 22:56:33 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'technics-cache-v1';
+const ASSETS = [
+  'select.html',
+  'img/Technics-Logo-org.png',
+  'img/favicon.png',
+  'manifest.json'
+];
+
+// Installation du Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Stratégie de cache : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
